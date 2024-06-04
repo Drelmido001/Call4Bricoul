79355509b956cd8313abc23f1d85bdf5394af720
Drelmido001 <elmehdidouiri99@gmail.com>
2024-06-04 19:32:55 +0100

    Projet initiale

---
+++ src/main/java/org/example/call4brikoul/Call4BriKoulApplication.java
@@ -0,0 +1,13 @@
+package org.example.call4brikoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Call4BriKoulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Call4BriKoulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/call4brikoul/Call4BriKoulApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.call4brikoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Call4BriKoulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
